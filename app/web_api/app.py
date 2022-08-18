5c10fe93fa613f692744416315917c938b649b22
habospace <avatarokwow@gmail.com>
2022-08-18 10:20:30 +0200

    add arg and return type annotations

--- app/web_api/app.py
+++ app/web_api/app.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from web_api.views import Loan
 
 
-def create_app(api_title: str, api_version: str, openapi_version: str, db_connection_string: str):
+def create_app(api_title: str, api_version: str, openapi_version: str, db_connection_string: str) -> Flask:
     app = Flask(__name__)
     app.config["API_TITLE"] = api_title
     app.config["API_VERSION"] = api_version
@@ -14,7 +14,7 @@ def create_app(api_title: str, api_version: str, openapi_version: str, db_connec
     return app
 
 
-def register_views(app):
+def register_views(app: Flask):
     loan_view = Loan.as_view("loan")
     # keeping it simple so registering /api/version/ prefixed blueprints
     app.add_url_rule("/loan/", defaults={"loan_id": None}, view_func=loan_view, methods=["GET"])
--- app/web_api/views.py
+++ app/web_api/views.py
@@ -1,9 +1,10 @@
+from typing import Dict, List
 from flask.views import MethodView
 
 
 class Loan(MethodView):
 
-    def get(self, loan_id):
+    def get(self, loan_id: int) -> Dict[str, List]:
         if loan_id is None:
             # return a list of users
             return {"loans": []}
@@ -11,15 +12,15 @@ class Loan(MethodView):
             # expose a single user
             return {"data": []}
 
-    def post(self):
+    def post(self) -> int:
         # create a new user
         loan_id = 0
         return loan_id
 
-    def delete(self, loan_id):
+    def delete(self, loan_id: int) -> int:
         # delete a single user
         return loan_id
 
-    def put(self, loan_id):
+    def put(self, loan_id: int) -> int:
         # update a single user
-        return id
+        return loan_id
