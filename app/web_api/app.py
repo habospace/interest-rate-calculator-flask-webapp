3fb004896d92d6a616da573a24b8d1182ba060ff
habospace <avatarokwow@gmail.com>
2022-08-22 03:29:40 +0200

    only register blueprint

--- app/web_api/app.py
+++ app/web_api/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from sqlalchemy import create_engine
+from flask_smorest import Api, Blueprint, abort
 
-from web_api.views import Loan
+from web_api.views import api_blp
 
 
 def create_app(api_title: str, api_version: str, openapi_version: str, db_connection_string: str) -> Flask:
@@ -10,13 +11,6 @@ def create_app(api_title: str, api_version: str, openapi_version: str, db_connec
     app.config["API_VERSION"] = api_version
     app.config["OPENAPI_VERSION"] = openapi_version
     app.db_connection = create_engine(db_connection_string)
-    register_views(app)
+    api = Api(app)
+    api.register_blueprint(api_blp)
     return app
-
-
-def register_views(app: Flask):
-    loan_view = Loan.as_view("loan")
-    # keeping it simple so registering /api/version/ prefixed blueprints
-    app.add_url_rule("/loan/", defaults={"loan_id": None}, view_func=loan_view, methods=["GET"])
-    app.add_url_rule("/loan/", view_func=loan_view, methods=["POST"])
-    app.add_url_rule("/loan/<int:loan_id>", view_func=loan_view, methods=["GET", "PUT", "DELETE"])
