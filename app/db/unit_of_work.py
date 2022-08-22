7110a0c047dacaea09d2fef86216dd5c28e9fde7
habospace <avatarokwow@gmail.com>
2022-08-22 03:30:27 +0200

    add unit of work class for managing database sessions

---
+++ app/db/unit_of_work.py
@@ -0,0 +1,27 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class UnitOfWork:
+
+    def __init__(self, connection=None):
+        self.connection = connection or create_engine(os.environ["DB_CONNECTION_STRING"])
+        self.session_maker = sessionmaker(bind=connection)
+
+    def __enter__(self):
+        self.session = self.session_maker()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            self.rollback()
+            self.session.close()
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
