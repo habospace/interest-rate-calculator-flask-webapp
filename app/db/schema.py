df93b7d535d35cc176bb3639bd6cf59aec1c0741
habospace <avatarokwow@gmail.com>
2022-08-23 21:01:43 +0200

    add db schema for daily base interest rates

--- app/db/schema.py
+++ app/db/schema.py
@@ -1,5 +1,8 @@
 import os
 from sqlalchemy import (
+    ForeignKey,
+    UniqueConstraint,
+    Index,
     create_engine,
     Integer,
     DECIMAL,
@@ -8,31 +11,52 @@ from sqlalchemy import (
     DateTime,
     Date,
 )
-from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_json import mutable_json_type
 from datetime import datetime
 
 engine = create_engine(os.environ["DB_CONNECTION_STRING"])
 Base = declarative_base()
 
 
+class BaseInterestRate(Base):
+    __tablename__ = "base_interest_rate"
+    id = Column(Integer, primary_key=True)
+    currency = Column(String(3), nullable=False)
+    date = Column(Date(), nullable=False)
+    interest_rate = Column(DECIMAL(precision=25, scale=10), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('currency', 'date', name='currency_date'),
+        Index('currency_date_index', "currency", "date")
+    )
+
+
 class Loan(Base):
     __tablename__ = 'loan'
     id = Column(Integer, primary_key=True)
 
-    amount = Column(DECIMAL(precision=20, scale=2), nullable=False)
+    amount = Column(DECIMAL(precision=25, scale=10), nullable=False)
     currency = Column(String(3), nullable=False)
-    base_interest_rate = Column(DECIMAL(precision=20, scale=2), nullable=False)
-    margin = Column(DECIMAL(precision=20, scale=2), nullable=False)
-
+    base_interest_rate = Column(DECIMAL(precision=25, scale=10), nullable=False)
+    margin = Column(DECIMAL(precision=25, scale=10), nullable=False)
     start_date = Column(Date(), default=datetime.now, nullable=False)
     end_date = Column(Date(), default=datetime.now, nullable=False)
 
-    calculation_result = Column(mutable_json_type(dbtype=JSON, nested=True))
+    total_interest = Column(DECIMAL(precision=25, scale=10), nullable=False)
 
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+
+class LoanCalculationResult(Base):
+    __tablename__ = "loan_calculation_result"
+    id = Column(Integer, primary_key=True)
+    loan_id = Column(Integer, ForeignKey("loan.id"), nullable=False, index=True)
+
+    date = Column(Date(), nullable=False)
+    interest_accrual_amount = Column(DECIMAL(precision=25, scale=10), nullable=False)
+    interest_accrual_amount_without_margin = Column(DECIMAL(precision=25, scale=10), nullable=False)
+    days_elapsed_since_loan_start_date = Column(Integer, nullable=False)
 
 
 Base.metadata.create_all(engine)
